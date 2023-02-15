58fbfa39f8157337c50a7903a7de73f34e17e71f
Ashish <Ashish9471847@gmail.com>
2023-02-15 16:11:06 +0530

    Commenting

--- 14FEB2023_Ashish/graph.py
+++ 14FEB2023_Ashish/graph.py
@@ -2,8 +2,9 @@
 #Author - Ashish kumar singh 
 #Created on - 14-02-2023
 
-
+#Function for make statructure of graph
 def graph(x):
+    #Given weight value of the weight values
     weight = {
     ('A', 'B') : 2,
     ('B', 'C') : 4,
@@ -15,11 +16,12 @@ def graph(x):
     ('A', 'C') : 8,
     ('A', 'D') : 6
 }
+    #Login for finding tthe all the possible combinations for the given coordinates
     T=[]
+    #Lenght of the given array
     l=len(x)
     s=0
     for i  in range(0,l):
-        
         for j in range(s,l):
             if i == j:
                 print("")
@@ -29,8 +31,8 @@ def graph(x):
                 t.append(x[j])
                 m=tuple(t)
                 T.append(m)
-                #print(T)
         s=s+1
+    #Logic for assigning the value of the given coordinates 
     lenth =len(T)
     lent =len(weight)
     out={}
@@ -39,11 +41,9 @@ def graph(x):
         out[T[k]]=0
         for n in range(c,lent):
             if T[k] == list(weight.keys())[n]:
-               out[list(weight.keys())[n]]= weight[list(weight.keys())[n]]
-            
-                  
-                 
+               out[list(weight.keys())[n]]= weight[list(weight.keys())[n]]  
+    #Printing the output           
     print(out)
-
+#Given input to be find the structure
 x=["A","B","C","D","E","F"]
 graph(x)
