91ccfcb96ff41674667bab2015b1434c03e5c5d2
Ashish <Ashish9471847@gmail.com>
2023-02-20 15:43:30 +0530

    ..

--- 17_FEB_2023_C4_Ashish/experiment.py
+++ 17_FEB_2023_C4_Ashish/experiment.py
@@ -2,6 +2,27 @@
 #Author - Ashish kumar singh 
 #Created on - 17    -02-2023
 
+#Base theorem
+def base(s,r,pro):
+    l=len(x)-1
+    n=0
+    m=0
+    for i in range(0,l):
+        
+        if s[i] == r[0] and s[i+1] == r[1]:
+            n=n+1
+            
+        if s[i] == r[1] and s[i+1] == r[0]:
+            m=m+1
+
+    a= (n-pro[0])/pro[1] 
+    b= (m-pro[1])/pro[0] 
+    
+    print("Prob of",r[0]," after Blue :",a) 
+    print("Prob of",r[1]," after Blue :",b) 
+     
+
+    
 #Function for Count the array elements and find probablity   
 def prob(x):
     sum=0
@@ -14,6 +35,7 @@ def prob(x):
         c=x.count(i)
         #Getting probablity
         pro=c/l
+
         print("Probablity of",i,"is:",pro)
         sum=sum+pro
     print("Verification of the output :",sum)
