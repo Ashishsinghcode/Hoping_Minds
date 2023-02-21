3b41c7e204d0c29754379b9eaff563be65cb1532
Ashish <Ashish9471847@gmail.com>
2023-02-21 16:41:46 +0530

    plotting

--- 17_FEB_2023_C4_Ashish/experiment.py
+++ 17_FEB_2023_C4_Ashish/experiment.py
@@ -2,6 +2,10 @@
 #Author - Ashish kumar singh 
 #Created on - 17    -02-2023
 
+#Importing libraries
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
 #Base theorem
 def base(s,r,pro):
     l=len(x)-1
@@ -24,21 +28,39 @@ def base(s,r,pro):
 
     
 #Function for Count the array elements and find probablity   
-def prob(x):
-    sum=0
+def prob(n):
+    sum=[]
     #COnvert into set and then list
-    s =list(set(x))
+    s =list(set(n))
     #Lenght of the array input by user
-    l=len(x)
+    l=len(n)
     for i in s:
         #COunt the specific element
-        c=x.count(i)
+        c=n.count(i)
         #Getting probablity
         pro=c/l
 
         print("Probablity of",i,"is:",pro)
-        sum=sum+pro
-    print("Verification of the output :",sum)
+        sum.append(pro)
+    coun=len(s)
+    print(coun)
+    l=[0,1]
+    #############################################################
+    data={'Element':s,'Value':sum}
+    total = pd.DataFrame(data)
+    print(total)
+    plt.hist(total,bins=coun)
+    
+    # giving labels to x-axis and y-axis
+   
+    
+    # giving title to the plot
+    
+    
+    # function to show plot
+    plt.show()
+    #import the seaborn library
+    ############################################################
 #Input by user
 x = list(map(str,input("Enter the elements").strip().split()))
 #Calling of function
