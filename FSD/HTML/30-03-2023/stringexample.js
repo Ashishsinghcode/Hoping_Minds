66c7025613f892014d50bd3a0a450dc7d7dadf72
Ashish <Ashish9471847@gmail.com>
2023-04-05 16:17:08 +0530

    llast commit

---
+++ FSD/HTML/30-03-2023/stringexample.js
@@ -0,0 +1,19 @@
+// Problem 1
+
+// a=10
+// a=String(a)
+// console.log(typeof((a))+" "+a)
+// a=Number(a)
+// console.log(typeof((a))+" "+a)
+
+//Problem 2
+//a="Yar bhai udhar dede Rs. 1000 , kal wapis deduga."
+// console.log(a.slice(24,28))
+
+//Problem 3
+// b=a.includes("100")
+// console.log(b)
+
+// a="Mohit"
+// a=a.replace("M","R")
+// console.log(a)
